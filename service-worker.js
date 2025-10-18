cbd7f4f07edc53608a015ae48f4fc8cdca2bb578
riaacordero <riarcordero@gmail.com>
2025-10-18 17:38:36 +0800

    Minor fixes

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,33 @@
 const CACHE_NAME = 'pickr-v1';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/script.js',
-  '/assets/pickr.svg',
-  '/assets/pop.mp3',
-  '/assets/icon-192.png',
-  '/assets/icon-512.png'
+  './',
+  './index.html',
+  './script.js',
+  './manifest.json',
+  './assets/pickr.svg',
+  './assets/pop.mp3',
+  './assets/icon-192.png',
+  './assets/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
